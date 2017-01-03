a429fb0d777c3381ecc863a1ccb20cba2c9a0e68
AlmoGSB <AlmoGSB@AlmoGSB.mynet>
2017-01-03 21:24:11 +0200

    //almog update

---
+++ Project/src/Controller/UserController.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import command.searchCommand;
+import client.DBSQLhandler;
+import Book.book;
+
+public class UserController {
+
+/*	public static ArrayList<user> SearchBook(book b,String condition,DBSQLhandler client)
+	{
+		// filed is need to look like "bookID,author,..."
+		client.searchInDB(new insertCommand<User>("user",condition, "userID, author"));//call command and client ask to search a book
+		while(!client.GetGotMessag()){//search book in db
+			try{
+			Thread.sleep(500);
+			}
+			catch(InterruptedException ex)
+			{
+				System.out.println("InterruptedException "+ex);
+			}
+		}
+		try {
+			return (ArrayList<book>)client.getResultObject();
+		} catch (SQLException e) {
+			return null;
+		}
+	}*/
+}
+
