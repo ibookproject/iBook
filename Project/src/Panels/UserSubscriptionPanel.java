00052e1255c095f14ec558a5e14669ac97e68f68
AlmoGSB <AlmoGSB@AlmoGSB>
2017-01-13 13:22:05 +0200

    user subscription panel - almog

--- Project/src/Panels/UserSubscriptionPanel.java
+++ Project/src/Panels/UserSubscriptionPanel.java
@@ -25,50 +25,101 @@ import Role.UserStatus;
 
 import java.awt.Font;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.spi.CalendarNameProvider;
 import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.awt.event.ActionEvent;
 
 public class UserSubscriptionPanel extends JPanel{
 	private ArrayList<Book> Books;
 	private LoginGUI screen;
 	private JLabel lblAnswerfromserver;
+//	private java.util.Date date;
 	
 	public UserSubscriptionPanel(LoginGUI screen,User u) {
 		setBackground(Color.WHITE);
 		setBorder(new MatteBorder(3, 3, 3, 3, (Color) new Color(46, 139, 87)));
-		setPreferredSize(new Dimension(577, 112));
+		setPreferredSize(new Dimension(577, 121));
 		setLayout(null);
-		
-		JButton btnSetSubscription = new JButton("Confirm");
-		btnSetSubscription.addActionListener(new ActionListener() {
+		JButton btnConfirmSubscription = new JButton("Confirm");
+		JButton btnRejectSubscription = new JButton("Reject");
+
+
+		btnConfirmSubscription.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 						boolean result = false;
 						if(u==null)
-							JOptionPane.showMessageDialog(screen,"Update user process FAILED ! ", "Warning",JOptionPane.WARNING_MESSAGE);
+							JOptionPane.showMessageDialog(screen,"Set subscription process FAILED!", "Warning",JOptionPane.WARNING_MESSAGE);
 						else
 						{
-							// update user by the new subscription method from the radio button that choosen
+							Calendar time = Calendar.getInstance();
+							time.setTime(time.getTime());
+							int method = u.getSubscriptionRequest();
+							switch(method){
+								case UserStatus.SINGLE:
+									time.add(Calendar.DATE, 1);
+									break;
+								case UserStatus.MONTHLY:
+									time.add(Calendar.MONTH, 1);
+									break;
+								case UserStatus.YEARLY:
+									time.add(Calendar.YEAR, 1);
+									break;
+							}
+							// update user by the new subscription method
 							// and set privilege to 2 (READER) if its under 2
 							if(u.getPriviliege() == 1)
-								result = UserController.SetStatusSubscription(u, "subscriptionMethod=\""+u.getSubscriptionRequest()+"\" && privilege=\""+ UserStatus.READER +"\"" + " && subscriptionRequest=\""+ 0 +"\"", "userID=\""+u.getUserID()+"\"", screen.getClient());
+								result = UserController.SetStatusSubscription(u, "subscriptionMethod=\""+u.getSubscriptionRequest()+"\" && privilege=\""+ UserStatus.READER +"\"" + " && subscriptionRequest=\""+ 0 +"\"" + " && finishDateOfSubscription=\"" + new SimpleDateFormat("yyyy/MM/dd").format(time.getTime()) + "\"" , "userID=\""+u.getUserID()+"\"", screen.getClient());
 							else
-								result = UserController.SetStatusSubscription(u, "subscriptionMethod=\""+u.getSubscriptionRequest()+"\""+ " && subscriptionRequest=\""+ 0 +"\"", "userID=\""+u.getUserID()+"\"", screen.getClient());
+								result = UserController.SetStatusSubscription(u, "subscriptionMethod=\""+u.getSubscriptionRequest()+"\""+ " && subscriptionRequest=\""+ 0 +"\"" + " && finishDateOfSubscription=\"" + new SimpleDateFormat("yyyy/MM/dd").format(time.getTime()) + "\"", "userID=\""+u.getUserID()+"\"", screen.getClient());
 							if(result)
 							{
-								lblAnswerfromserver.setText("Updated successfully!");
+								//lblAnswerfromserver.setText("Confirmed successfully!");
+								lblAnswerfromserver.setText("Confirmed successfully!");
 								lblAnswerfromserver.setForeground(Color.GREEN);
-								btnSetSubscription.setVisible(false);
+								btnConfirmSubscription.setVisible(false);
+								btnRejectSubscription.setVisible(false);
 							}
 							else 
-								JOptionPane.showMessageDialog(screen,"Update user process FAILED", "Warning",JOptionPane.WARNING_MESSAGE);
+								JOptionPane.showMessageDialog(screen,"Set subscription process FAILED!", "Warning",JOptionPane.WARNING_MESSAGE);
 
 							
 						}
 					
 			}
 		});
-		btnSetSubscription.setBounds(415, 32, 117, 23);
-		add(btnSetSubscription);
+		
+
+		btnRejectSubscription.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+						boolean result = false;
+						if(u==null)
+							JOptionPane.showMessageDialog(screen,"Set subscription process FAILED!", "Warning",JOptionPane.WARNING_MESSAGE);
+						else
+						{
+							// update user to reject subscription
+							result = UserController.SetStatusSubscription(u, "subscriptionMethod=\""+0+"\"" + " && subscriptionRequest=\""+ 0 +"\"", "userID=\""+u.getUserID()+"\"", screen.getClient());
+						if(result)
+							{
+								lblAnswerfromserver.setText("Rejected successfully!");
+								lblAnswerfromserver.setForeground(Color.RED);
+								btnConfirmSubscription.setVisible(false);
+								btnRejectSubscription.setVisible(false);
+							}
+							else 
+								JOptionPane.showMessageDialog(screen,"Set subscription process FAILED!", "Warning",JOptionPane.WARNING_MESSAGE);
+
+							
+						}
+					
+			}
+		});
+		btnRejectSubscription.setBounds(469, 32, 87, 23);
+		add(btnRejectSubscription);
+		btnConfirmSubscription.setBounds(369, 32, 87, 23);
+		add(btnConfirmSubscription);
 		
 		JLabel lblUserid = new JLabel("UserID:");
 		lblUserid.setBounds(22, 11, 147, 14);
@@ -114,16 +165,17 @@ public class UserSubscriptionPanel extends JPanel{
 
 		JLabel lblRequestsubscriptiondb = new JLabel(choose);
 		lblRequestsubscriptiondb.setForeground(Color.BLUE);
-		lblRequestsubscriptiondb.setBounds(179, 83, 141, 14);
+		lblRequestsubscriptiondb.setBounds(179, 86, 141, 14);
 		add(lblRequestsubscriptiondb);
 		
-		lblAnswerfromserver = new JLabel("");
-		lblAnswerfromserver.setBounds(415, 66, 152, 14);
+		lblAnswerfromserver = new JLabel();
+		lblAnswerfromserver.setBounds(379, 66, 152, 14);
 		add(lblAnswerfromserver);
-
+		
 			
 	}
 }
-
+//how to print the date right now:
+//String dateRightNow = new SimpleDateFormat("yyyy/MM/dd").format(cal.getTime());
 
 
