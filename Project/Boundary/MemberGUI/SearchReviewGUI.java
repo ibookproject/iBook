7fa219dfa8e085483e8a648db63f456a9af6d745
hensaada <hensaada@hen>
2017-01-26 11:30:31 +0200

    search review - new

--- Project/Boundary/MemberGUI/SearchReviewGUI.java
+++ Project/Boundary/MemberGUI/SearchReviewGUI.java
@@ -9,20 +9,14 @@ import javax.swing.border.MatteBorder;
 import Book.Book;
 import Book.Review;
 import Controller.ReviewController;
-import Controller.UserController;
 import Extras.Validation;
 import Controller.BookController;
 import MenuGUI.LoginGUI;
-import Panels.BookPanel;
-import Panels.ReviewPanel;
 import Panels.SearchReviewPanel;
-import Panels.UserSubscriptionPanel;
-import Role.User;
-
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
@@ -33,6 +27,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javax.swing.JRadioButton;
 
 public class SearchReviewGUI extends JPanel {
 	private JTextField textFieldAutohr;
@@ -41,6 +36,7 @@ public class SearchReviewGUI extends JPanel {
 	private ArrayList<Book> tempBooks;
 	private LoginGUI screen;
 	private Review r;
+	private int radioButtonChoose = 0;// integer to sent what the radio button
 	private JPanel pann;
 	public static JPanel panel;
 	private JScrollPane scrollPaneMain;
@@ -51,6 +47,7 @@ public class SearchReviewGUI extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
 	public JButton btnBack ;
+	private JTextField textFieldKeyWord;
 	
 	public SearchReviewGUI(LoginGUI screen) {
 		super();
@@ -67,34 +64,97 @@ public class SearchReviewGUI extends JPanel {
 		this.setSize(850, 625);
 		this.setLayout(null);	
 		
+		JLabel lblKeyWord = new JLabel("Key Word : ");
+		lblKeyWord.setBounds(334, 101, 73, 19);
+		add(lblKeyWord);
+		
+		textFieldKeyWord = new JTextField();
+		textFieldKeyWord.setBounds(406, 100, 86, 20);
+		add(textFieldKeyWord);
+		textFieldKeyWord.setColumns(10);
+		
 		JLabel lblNewLabel = new JLabel("Search Review's of book");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		lblNewLabel.setBounds(352, 59, 211, 14);
 		add(lblNewLabel);
 		
 		JLabel lblNameOfAuthor = new JLabel("name of author:");
-		lblNameOfAuthor.setBounds(400, 90, 89, 19);
+		lblNameOfAuthor.setBounds(513, 88, 89, 19);
 		add(lblNameOfAuthor);
 		
 		textFieldAutohr = new JTextField();
-		textFieldAutohr.setBounds(499, 89, 86, 20);
+		textFieldAutohr.setBounds(614, 87, 86, 20);
 		add(textFieldAutohr);
 		textFieldAutohr.setColumns(10);
 		
-		JLabel lblNameOfBook = new JLabel("name of book:");
-		lblNameOfBook.setBounds(213, 94, 111, 19);
+		JLabel lblNameOfBook = new JLabel("title of book:");
+		lblNameOfBook.setBounds(334, 88, 111, 19);
 		add(lblNameOfBook);
 		
 		textFieldBook = new JTextField();
-		textFieldBook.setBounds(300, 90, 86, 20);
+		textFieldBook.setBounds(417, 87, 86, 20);
 		add(textFieldBook);
 		textFieldBook.setColumns(10);
 		
-		
 		JComboBox comboBoxOfBooks = new JComboBox();
+		comboBoxOfBooks.setBounds(360, 118, 348, 20);
+		add(comboBoxOfBooks);
 		
+		
+		JRadioButton rdbtnSearchByTitleauthor = new JRadioButton("Search By Title/Author");
+		rdbtnSearchByTitleauthor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				radioButtonChoose=1;
+				panel.removeAll();
+				panel.updateUI();
+				comboBoxOfBooks.setVisible(true);
+				lblNameOfBook.setVisible(true);
+				textFieldBook.setVisible(true);
+				lblNameOfAuthor.setVisible(true);
+				textFieldAutohr.setVisible(true);
+				lblKeyWord.setVisible(false);
+				textFieldKeyWord.setVisible(false);
+			}
+		});
+		rdbtnSearchByTitleauthor.setBounds(151, 86, 158, 23);
+		add(rdbtnSearchByTitleauthor);
+		
+		JRadioButton rdbtnSearchByBook = new JRadioButton("Search By Book KeyWord");
+		rdbtnSearchByBook.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				radioButtonChoose=2;
+				panel.removeAll();
+				panel.updateUI();
+				comboBoxOfBooks.setVisible(false);
+				lblNameOfBook.setVisible(false);
+				textFieldBook.setVisible(false);
+				lblNameOfAuthor.setVisible(false);
+				textFieldAutohr.setVisible(false);
+				lblKeyWord.setVisible(true);
+				textFieldKeyWord.setVisible(true);
+			}
+		});
+		rdbtnSearchByBook.setBounds(151, 117, 185, 23);
+		add(rdbtnSearchByBook);
+		
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnSearchByTitleauthor);
+		group.add(rdbtnSearchByBook);
+		
+		rdbtnSearchByBook.setSelected(true);
+		comboBoxOfBooks.setVisible(false);
+		lblNameOfBook.setVisible(false);
+		textFieldBook.setVisible(false);
+		lblNameOfAuthor.setVisible(false);
+		textFieldAutohr.setVisible(false);
+		lblKeyWord.setVisible(true);
+		textFieldKeyWord.setVisible(true);
+
 		comboBoxOfBooks.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				panel.removeAll();
+				panel.updateUI();
 				 index=comboBoxOfBooks.getSelectedIndex();
 				if (index!=-1)
 					bookId=tempBooks.get(index).getBookID();
@@ -130,9 +190,8 @@ public class SearchReviewGUI extends JPanel {
 			}
 		});
 		
-		comboBoxOfBooks.setBounds(223, 120, 412, 20);
-		add(comboBoxOfBooks);
-		
+
+
 		
 		scrollPaneMain = new JScrollPane();
 		scrollPaneMain.getVerticalScrollBar().setUnitIncrement(16);
@@ -150,22 +209,20 @@ public class SearchReviewGUI extends JPanel {
 		panel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
 		scrollPaneMain.setViewportView(panel);
 		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
 	
-		
-		
 		JButton btnSearch = new JButton("Search");
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-
-				
+				if(radioButtonChoose==1)
+				{			
 			 	 if(textFieldAutohr.getText().isEmpty()&&textFieldBook.getText().isEmpty())
 				 {
 		 				comboBoxOfBooks.removeAllItems();
 		 				panel.removeAll();
 		 				panel.setVisible(false);
 						scrollPaneMain.setVisible(false);
+						panel.updateUI();
 						JOptionPane.showMessageDialog(screen,"you must fill the name/author of the book !! ", "Warning",JOptionPane.WARNING_MESSAGE);
 				 }
 				 else
@@ -185,12 +242,14 @@ public class SearchReviewGUI extends JPanel {
 							scrollPaneMain.setVisible(false);
 								JOptionPane.showMessageDialog(screen,"no book results were found ", "Warning",JOptionPane.WARNING_MESSAGE);
 								textFieldBook.setText("");textFieldAutohr.setText("");
+								panel.updateUI();
 				 		 }
 				 		 else
 				 		 {
 				 				comboBoxOfBooks.removeAllItems();
 							for(int i=0;i<tempBooks.size();i++)
 								comboBoxOfBooks.addItem("Name: "+tempBooks.get(i).getTitle().trim() + " , " +"Author: "+ tempBooks.get(i).getAuthor().trim());
+							panel.updateUI();
 				 		 }	
 				 		} else 
 				 		{
@@ -212,6 +271,8 @@ public class SearchReviewGUI extends JPanel {
 							scrollPaneMain.setVisible(false);
 							JOptionPane.showMessageDialog(screen,"no book results were found ", "Warning",JOptionPane.WARNING_MESSAGE);
 							textFieldBook.setText("");textFieldAutohr.setText("");
+							panel.updateUI();
+
 			 		 }
 			 		 else
 			 		 {
@@ -238,6 +299,8 @@ public class SearchReviewGUI extends JPanel {
 							JOptionPane.showMessageDialog(screen,"no book results were found ", "Warning",JOptionPane.WARNING_MESSAGE);
 							textFieldBook.setText("");textFieldAutohr.setText("");
 							comboBoxOfBooks.removeAllItems();
+							panel.updateUI();
+
 			 		 }
 			 		 else
 			 		 {
@@ -248,16 +311,52 @@ public class SearchReviewGUI extends JPanel {
 			 		} else {JOptionPane.showMessageDialog(screen,"Iligal author field!! ", "Warning",JOptionPane.WARNING_MESSAGE);textFieldAutohr.setText("");}
 
 			 	}
+			}
+			 	 }
+				else
+				{	
+	 				panel.removeAll();
 
-			}}
+					 r = new Review();// create review
+					 ArrayList<Book> bookKeywordsChoose = new ArrayList<Book> ();
+					bookKeywordsChoose=BookController.searchKeywords(textFieldKeyWord.getText(),screen.getClient());
+					if(bookKeywordsChoose!=null){
+					for(int i=0;i<bookKeywordsChoose.size();i++)
+					{
+					temp = ReviewController.SearchReviews("reviewID,reviewDate,reviewContent,reviewStatus,bookID", r, ""+ "bookID=\""+bookKeywordsChoose.get(i).getBookID()+"\""+ " && "+"reviewStatus=\""+"1"+"\"", screen.getClient());
+					if (temp != null) 
+					{
+						panel.setVisible(true);
+						scrollPaneMain.setVisible(true);
+							for(int j=0;j<temp.size();j++)
+							{
+								panel.add(new SearchReviewPanel(screen,temp.get(j),bookKeywordsChoose.get(i).getTitle()));	
+							}
+					} 			
+					}
+					}
+					else 
+					{
+						panel.setVisible(false);
+						scrollPaneMain.setVisible(false);
+						JOptionPane.showMessageDialog(screen,"no review results !  ", "no results",JOptionPane.QUESTION_MESSAGE);
+						textFieldKeyWord.setText("");
+						
+					}
+					panel.updateUI();				
+				}
+			}
 			}); 
-		btnSearch.setBounds(612, 86, 89, 23);
+		btnSearch.setBounds(710, 84, 89, 23);
 		add(btnSearch);
 
 		ImageIcon backIcon =new ImageIcon("Extras/Images/backIcon.png");
 		btnBack = new JButton(backIcon);
 		btnBack.setBounds(39, 52, 89, 23);
 		add(btnBack);
+		
+
+
 
 	}
 }
